7b0238bd86e7031b1b3b46998d9bd2da84e98491
Debobrota Haldar <iamdebobrota@gmail.com>
2023-01-27 19:54:44 +0530

    added: custom hooks assignments

---
+++ Rct211/customhooks/src/hooks/useOnlineStatus.js
@@ -0,0 +1,19 @@
+import { useState,useEffect } from "react";
+
+const useOnlineStatus = () => {
+const [isUserOnline,setIsUserOnline] = useState(false)
+
+useEffect(()=>{
+    if (navigator.onLine) {
+    console.log('online');
+    setIsUserOnline(true)
+  } else {
+    console.log('offline');
+    setIsUserOnline(false)
+  }
+},[navigator.onLine])
+  
+    return isUserOnline
+};
+
+export default useOnlineStatus;
---
+++ Rct211/customhooks/src/hooks/useTimeout.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+
+const useTimeout = (delay) => {
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setReady(true), delay);
+  }, []);
+
+  return { ready };
+};
+
+export default useTimeout;
